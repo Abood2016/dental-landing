c510898678ebe5fc5c0975e25185bf6bbcc67874
Abdulrahamn Kabar <kabar.abdullrahman@gmail.com>
2021-07-01 13:57:40 +0300

    update service page

--- resources/views/front/service_detail.blade.php
+++ resources/views/front/service_detail.blade.php
@@ -43,10 +43,52 @@
     </div>
 </nav>
  <main>
-
+    <div class="container-fluid service-content">
+        <div class="row">
+            <div class="col-sm-6 text-center">
+                <img class="service-image" src="{{asset('front_assets/images/service_three.jpg')}}" alt="">
+            </div>
+            <div class="col-sm-6 row d-flex flex-column">
+            <div class="col-sm-12 title-content-service">
+                طب الأسنان الترميمي
+            </div>
+                <div class="col-sm-12  text-justify content-contetn-service">
+                    شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح شرح
+                </div>
+            </div>
+        </div>
+    </div>
+     <br>
+     <br>
+     <br>
  </main>
 <footer class="container-fluid" style="background-color: #18afd3">
-    @include('front.layouts.footer')
+    <div class="row">
+        <div class="col-sm-5 d-flex justify-content-center footer-logo-mobile" >
+            <img src="{{ URL::asset('/images/logo/'. $setting->logo )}}" class="footer-logo" width="206px" height="63px"
+                 alt="">
+        </div>
+        <div class="col-sm-4 all-right d-flex flex-column">
+            جميع الحقوق محفوظة.
+            <a href="/" class=" col-5 text-center mt-2 link-footer">
+                لدى G.M.G
+            </a>
+        </div>
+        <div class="col-sm-3 d-flex flex-row pt-4 pb-4 mr">
+            <a href="{{ $setting->facebook_url }}" style="" class="social-icon">
+            <span class="fab fa-facebook-f social-itself">
+            </span>
+            </a>
+            <a href="{{ $setting->twitter_url}}" style="" class="social-icon">
+            <span class="fab fa-twitter social-itself">
+            </span>
+            </a>
+            <a href="{{ $setting->instagram_url }}" style="" class="social-icon">
+            <span class="fab fa-instagram social-itself">
+            </span>
+            </a>
+        </div>
+    </div>
 </footer>
 
 </body>
